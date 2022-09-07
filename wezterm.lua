0204f9bce833bc6b9c42c88966801a11f6fbd1ff
mui-z <93278577+mh-idea@users.noreply.github.com>
2022-09-07 23:27:04 +0900

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm';
+
+return {
+  use_ime = true,
+  font_size = 11.0,
+  color_scheme = "iceberg-light", 
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+  colors = {
+    background ='#fafafa'
+  }
+}
+
